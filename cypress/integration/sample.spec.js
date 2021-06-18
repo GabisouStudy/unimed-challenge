a7aeac864a629ee10929daf118989c491fe80346
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-06-18 10:33:03 -0300

    Search by "cidade" and "especialidade"
    - Added flow in text to do a search with the "cidade" and "especialidade" fields
    - Removed Cypress examples of tests
    - Updated Cypress to version 7.5.0

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -17,12 +17,23 @@ describe('Unimed', ()=>{
 
 	it('has a title', ()=>{
 		//expect(2).to.equal(2);
-		cy.visit("http://www.unimed.coop.br");
+		cy.visit("https://www.unimed.coop.br");
 
 		//cy.contains("Guia Médico:visible");
 
 		//cy.filter(":contains(Guia Médico)");
+		cy.log("adding intercept");
+		//cy.intercept("GET", "https://www.cloudflare.com/cdn-cgi/trace").as("trace");
+		//cy.wait("@getEspecs", { timeout: 2000 });
 		cy.get("a").filter(":contains(Guia Médico)").filter(":visible").first().click();
+		//cy.wait(6000);
 		//cy.get("h2").filter(":contains(Guia Médico)").click();
+
+		cy.contains("Busca detalhada").click();
+		cy.get("#province-input > div > div.css-5ahn1r > div.css-tlfecz-indicatorContainer > svg").click();
+		cy.contains("Rio de Janeiro").click();
+		cy.get("#react-tabs-3 > form > div > div:nth-child(3) > div:nth-child(1) > div > div > div.css-5ahn1r > div > svg").click();
+		cy.contains("Alergia e Imunologia").click();
+		cy.contains("Pesquisar").click();
 	})
 })
