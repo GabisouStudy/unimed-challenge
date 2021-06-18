2bee8beea7077fdfbd9460541088e2e9a3585255
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-06-18 14:49:40 -0300

    Checking if the filed "cidade" of the results is not São Paulo
    - Get, filter and asserts for the "cidade" field to not contains São Paulo

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -39,5 +39,36 @@ describe('Unimed', () => {
 		.each(($elem) => {
 			cy.wrap($elem).should("contains.text", "Alergia e Imunologia");
 		});
-	})
+	});
+
+	it("Checking if it's not São Paulo", ()=>{
+		cy.get("a")
+		.filter(":contains(Guia Médico)")
+		.filter(":visible")
+		.first()
+		.click();
+
+		cy.contains("Busca detalhada").click();
+
+		cy.wait(5000);
+
+		cy.get(
+			"#province-input > div > div.css-5ahn1r > div.css-tlfecz-indicatorContainer > svg"
+		).click();
+		cy.contains("Rio de Janeiro").click();
+		cy.get(
+			"#react-tabs-3 > form > div > div:nth-child(3) > div:nth-child(1) > div > div > div.css-5ahn1r > div > svg"
+		).click();
+		cy.contains("Alergia e Imunologia").click();
+		cy.contains("Pesquisar").click();
+
+		cy.wait(5000);
+
+		cy.get("a")
+		.filter(".ProviderAddressAsGrid--address-link")
+		.each(($elem) => {
+			cy.wrap($elem).should("not.contains.text", "São Paulo");
+		});
+		// TODO: Check the multiple pages (pages 1,2 and 3)
+ 	});
 });
