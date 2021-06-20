d3545684b9de7bc1a2a8fa1e288da3d4b6f39690
Gabriel de Souza <devgabrieldesouza@gmail.com>
2021-06-20 11:19:30 -0300

    Tests with arguments as variables
    - Added arguments to the tests descriptions in Cucumber
    - Added paramters int the tests functions to receive the arguments from Cucumber
    - Added VS Code cache folder to .gitignore

--- cypress/integration/GuiaMedico.feature
+++ cypress/integration/GuiaMedico.feature
@@ -1,21 +1,17 @@
-# languange: pt
-
 Feature: Guia Médico
-	
-	Background: Usuário acessa o site da Unimed
-		Given que eu acessei a página da Unimed
+
+	Background: Usuário acessa a página de Busca detalhada no site da Unimed
+		Given que o usuário acessa o site da Unimed
+		And acessa a página de "Guia Médico"
+		And ativa a aba "Busca detalhada"
 
 	@focus
 	Scenario: Busca por médicos deve retornar especialidade e cidade inseridas pelo usuário
-		Given que estou na página de Guia Médico
-		And com a aba Busca detalhada ativada
-		When eu busco pela especialidade Alergia e Imunologia e cidade Rio de Janeiro
-		Then todos os resultados devem conter Alergia e Imunologia no campo de especialidade
-		And todos os resultados devem conter Rio de Janeiro no campo de cidade
+		When eu busco pela especialidade "Alergia e Imunologia" e cidade "Rio de Janeiro"
+		Then todos os resultados devem conter "Alergia e Imunologia" no campo de especialidade
+		And todos os resultados devem conter "Rio de Janeiro" no campo de cidade
 
 	@focus
 	Scenario: Busca por médicos não pode retornar um resultado com uma cidade diferente da inserida pelo usuário
-		Given que estou na página de Guia Médico
-		And com a aba Busca detalhada ativada
-		When eu busco pela especialidade Alergia e Imunologia e cidade Rio de Janeiro
-		Then nenhum resultado nas 3 primeiras paáginas devem conter São Paulo no campo de cidade
+		When eu busco pela especialidade "Alergia e Imunologia" e cidade "Rio de Janeiro"
+		Then nenhum resultado nas "3" primeiras páginas devem conter "São Paulo" no campo de cidade
--- cypress/integration/GuiaMedico/steps.js
+++ cypress/integration/GuiaMedico/steps.js
@@ -1,65 +1,68 @@
-import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+/// <reference types="cypress" />
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-Given("que eu acessei a página da Unimed", () => {
+
+Given(/^que o usuário acessa o site da Unimed$/, () => {
 	cy.viewport(1920, 1080);
 	cy.visit("https://www.unimed.coop.br");
 });
 
-Given("que estou na página de Guia Médico", () => {
+And(/^acessa a página de "([^"]*)"$/, (pageName) => {
 	cy.get("a")
-	.filter(":contains(Guia Médico)")
+	.filter(":contains("+pageName+")")
 	.filter(":visible")
 	.first()
 	.click();
 });
 
-Then("com a aba Busca detalhada ativada", () => {
-	cy.contains("Busca detalhada")
+And(/^ativa a aba "([^"]*)"$/, (tabName) => {
+	cy.contains(tabName)
 	.click();
 });
 
-When("eu busco pela especialidade Alergia e Imunologia e cidade Rio de Janeiro", () => {
+When(/^eu busco pela especialidade "([^"]*)" e cidade "([^"]*)"$/,(especialidade, cidade) => {
 	cy.wait(5000);
 
 	cy.get("#province-input > div > div.css-5ahn1r > div.css-tlfecz-indicatorContainer > svg").click();
-	cy.contains("Rio de Janeiro").click();
+	cy.contains(cidade).click();
 	cy.get("#react-tabs-3 > form > div > div:nth-child(3) > div:nth-child(1) > div > div > div.css-5ahn1r > div > svg").click();
-	cy.contains("Alergia e Imunologia").click();
+	cy.contains(especialidade).click();
 	cy.contains("Pesquisar").click();
 });
 
-Then("todos os resultados devem conter Alergia e Imunologia no campo de especialidade", () => {
+And(/^todos os resultados devem conter "([^"]*)" no campo de especialidade$/,(especialidade) => {
 	cy.wait(5000);
 
 	cy.get("div")
 	.filter(".ProviderSpecialties")
 	.each(($elem) => {
-		cy.wrap($elem).should("contains.text", "Alergia e Imunologia");
+		cy.wrap($elem).should("contains.text", especialidade);
 	});cy.get("div")
 	.filter(".ProviderSpecialties")
 	.each(($elem) => {
-		cy.wrap($elem).should("contains.text", "Alergia e Imunologia");
+		cy.wrap($elem).should("contains.text", especialidade);
 	});
 });
 
-Then("todos os resultados devem conter Rio de Janeiro no campo de cidade", () => {
+Then(/^todos os resultados devem conter "([^"]*)" no campo de cidade$/,(cidade) => {
 	cy.wait(5000);
 
 	cy.get("a")
 	.filter(".ProviderAddressAsGrid--address-link")
 	.each(($elem) => {
-		cy.wrap($elem).should("contains.text", "Rio de Janeiro/RJ");
+		cy.wrap($elem).should("contains.text", cidade);
 	});
 });
 
-Then("nenhum resultado nas 3 primeiras paáginas devem conter São Paulo no campo de cidade", () => {
+
+Then(/^nenhum resultado nas "([^"]*)" primeiras páginas devem conter "([^"]*)" no campo de cidade$/, (pageAmount, cidade) => {
 	cy.wait(5000);
 
-	for(let i = 0; i < 3; i++){
+	for(let i = 0; i < parseInt(pageAmount); i++){
 		cy.get("a")
 		.filter(".ProviderAddressAsGrid--address-link")
 		.each(($elem) => {
-			cy.wrap($elem).should("not.contains.text", "São Paulo");
+			cy.wrap($elem).should("not.contains.text", cidade);
 		});
 		if(i == 3 - 1) break;
 		cy.contains("Ver mais resultados").click();
